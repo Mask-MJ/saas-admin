c2d7bba794f00a42bf8592205b60b241994c3547
mask <617473294@qq.com>
2024-07-09 17:34:55 +0800

    first commit

---
+++ src/views/dashboard/index.page.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// 重定向到首页
+import { useRouter } from 'vue-router';
+const router = useRouter();
+router.push('/dashboard/workTable');
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="" scoped></style>
