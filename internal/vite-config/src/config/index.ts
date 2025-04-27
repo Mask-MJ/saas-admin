c6ecdc71cae6551f90ea973afe05ec348845bc09
mask <617473294@qq.com>
2025-04-27 20:49:02 +0800

    feat: init

---
+++ internal/vite-config/src/config/index.ts
@@ -0,0 +1,33 @@
+import type { DefineConfig } from '../typing';
+
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { defineApplicationConfig } from './application';
+
+export * from './application';
+
+function defineConfig(
+  userConfigPromise?: DefineConfig,
+  type: 'application' | 'auto' | 'library' = 'auto',
+) {
+  let projectType = type;
+
+  // 根据包是否存在 index.html,自动判断类型
+  if (projectType === 'auto') {
+    const htmlPath = join(process.cwd(), 'index.html');
+    projectType = existsSync(htmlPath) ? 'application' : 'library';
+  }
+
+  switch (projectType) {
+    case 'application': {
+      return defineApplicationConfig(userConfigPromise);
+    }
+
+    default: {
+      throw new Error(`Unsupported project type: ${projectType}`);
+    }
+  }
+}
+
+export { defineConfig };
---
+++ internal/vite-config/src/plugins/index.ts
@@ -0,0 +1,156 @@
+import type { PluginOption } from 'vite';
+
+import type {
+  ApplicationPluginOptions,
+  CommonPluginOptions,
+  ConditionPlugin,
+  LibraryPluginOptions,
+} from '../typing';
+
+import viteVueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import viteVue from '@vitejs/plugin-vue';
+import viteVueJsx from '@vitejs/plugin-vue-jsx';
+import { visualizer as viteVisualizerPlugin } from 'rollup-plugin-visualizer';
+import viteDtsPlugin from 'vite-plugin-dts';
+import viteVueDevTools from 'vite-plugin-vue-devtools';
+
+import { viteExtraAppConfigPlugin } from './extra-app-config';
+import { viteInjectAppLoadingPlugin } from './inject-app-loading';
+import { viteLicensePlugin } from './license';
+import { vitePrintPlugin } from './print';
+
+/**
+ * 获取条件成立的 vite 插件
+ * @param conditionPlugins
+ */
+async function loadConditionPlugins(conditionPlugins: ConditionPlugin[]) {
+  const plugins: PluginOption[] = [];
+  for (const conditionPlugin of conditionPlugins) {
+    if (conditionPlugin.condition) {
+      const realPlugins = await conditionPlugin.plugins();
+      plugins.push(...realPlugins);
+    }
+  }
+  return plugins.flat();
+}
+
+/**
+ * 根据条件获取通用的vite插件
+ */
+async function loadCommonPlugins(
+  options: CommonPluginOptions,
+): Promise<ConditionPlugin[]> {
+  const { devtools, isBuild, visualizer } = options;
+  return [
+    {
+      condition: true,
+      plugins: () => [
+        viteVue({
+          script: {
+            defineModel: true,
+            // propsDestructure: true,
+          },
+        }),
+        viteVueJsx(),
+      ],
+    },
+
+    {
+      condition: !isBuild && devtools,
+      plugins: () => [viteVueDevTools()],
+    },
+    {
+      condition: isBuild && !!visualizer,
+      plugins: () => [<PluginOption>viteVisualizerPlugin({
+          filename: './node_modules/.cache/visualizer/stats.html',
+          gzipSize: true,
+          open: true,
+        })],
+    },
+  ];
+}
+
+/**
+ * 根据条件获取应用类型的vite插件
+ */
+async function loadApplicationPlugins(
+  options: ApplicationPluginOptions,
+): Promise<PluginOption[]> {
+  // 单独取，否则commonOptions拿不到
+  const isBuild = options.isBuild;
+  const env = options.env;
+
+  const {
+    extraAppConfig,
+    i18n,
+    injectAppLoading,
+    license,
+    print,
+    printInfoMap,
+    ...commonOptions
+  } = options;
+
+  const commonPlugins = await loadCommonPlugins(commonOptions);
+
+  return await loadConditionPlugins([
+    ...commonPlugins,
+    {
+      condition: i18n,
+      plugins: async () => {
+        return [
+          viteVueI18nPlugin({
+            compositionOnly: true,
+            fullInstall: true,
+            runtimeOnly: true,
+          }),
+        ];
+      },
+    },
+    {
+      condition: print,
+      plugins: async () => {
+        return [await vitePrintPlugin({ infoMap: printInfoMap })];
+      },
+    },
+    {
+      condition: injectAppLoading,
+      plugins: async () => [await viteInjectAppLoadingPlugin(!!isBuild, env)],
+    },
+    {
+      condition: license,
+      plugins: async () => [await viteLicensePlugin()],
+    },
+    {
+      condition: isBuild && extraAppConfig,
+      plugins: async () => [
+        await viteExtraAppConfigPlugin({ isBuild: true, root: process.cwd() }),
+      ],
+    },
+  ]);
+}
+
+/**
+ * 根据条件获取库类型的vite插件
+ */
+async function loadLibraryPlugins(
+  options: LibraryPluginOptions,
+): Promise<PluginOption[]> {
+  // 单独取，否则commonOptions拿不到
+  const isBuild = options.isBuild;
+  const { dts, ...commonOptions } = options;
+  const commonPlugins = await loadCommonPlugins(commonOptions);
+  return await loadConditionPlugins([
+    ...commonPlugins,
+    {
+      condition: isBuild && !!dts,
+      plugins: () => [viteDtsPlugin({ logLevel: 'error' })],
+    },
+  ]);
+}
+
+export {
+  loadApplicationPlugins,
+  loadLibraryPlugins,
+  viteDtsPlugin,
+  viteVisualizerPlugin,
+};
